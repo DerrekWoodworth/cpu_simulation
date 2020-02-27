27bf93211d952f475f4a8343c555a1346f6369e6
Cole Spitzer <baseballer.cs@gmail.com>
2020-02-26 22:02:00 -0500

    All Queues Made

--- src/main/java/implementations/MultilevelFeedbackScheduler.java
+++ src/main/java/implementations/MultilevelFeedbackScheduler.java
@@ -1,32 +1,54 @@
 package implementations;
 
+import java.util.ArrayList;
+
 import interfaces.CPUSchedulerInterface;
 
 public class MultilevelFeedbackScheduler implements CPUSchedulerInterface {
-    public void addProcess(Process toAdd) {
+	
+	ArrayList <Process> myProcesses = new ArrayList <Process>();
+	
+    public MultilevelFeedbackScheduler(ArrayList<Process> myProcesses) {
+		super();
+		this.myProcesses = myProcesses;
+	}
 
+	public void addProcess(Process toAdd) {
+    	myProcesses.add(toAdd);
     }
 
-    public Process getNextProcess() {
-        return null;
+    public Process getNextProcessFirst() {
+    	return myProcesses.get(0);
+    }
+    
+    public Process getNextProcessSecond() {
+    	return myProcesses.get(0);
+    }
+    
+    public Process getNextProcessBase() {
+    	return myProcesses.get(0);
     }
 
     public Process[] getAllFinishedProcesses() {
-        return new Process[0];
+    	return new Process[0];
     }
 
     public void firstQueue() {
-
+    	myProcesses.add(myProcesses.remove(0));
     }
     
     public void secondQueue() {
-    	
-    	
+    	myProcesses.add(myProcesses.remove(0));
     }
     
     public void baseQueue() {
-    	
+    	myProcesses.add(myProcesses.remove(0));
     }
+
+	public Process getNextProcess() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	public void schedule() {
 		// TODO Auto-generated method stub
